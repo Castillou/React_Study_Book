376fad1a370d1ba8a354882b04a018b9e46321d0
Castillou <joajoa70584@gmail.com>
2024-09-27 00:30:47 +0900

    NewsViewer add Router

--- news-viewer/src/components/NewsList.jsx
+++ news-viewer/src/components/NewsList.jsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
+import usePromise from '../lib/usePromise';
 
 import NewsItem from './NewsItem';
 
@@ -19,26 +19,11 @@ const NewsListBlock = styled.div`
 const API_KEY = 'a3b7a2f7f8b245439aaa7dcceeb6f115';
 
 function NewsList({ category }) {
-	const [articles, setArticles] = useState(null);
-	const [loading, setLoading] = useState(false);
-
-	useEffect(() => {
-		const fetchData = async () => {
-			setLoading(true);
-			try {
-				const query = category === 'all' ? '' : `&category=${category}`;
-
-				const response = await axios.get(
-					`https://newsapi.org/v2/top-headlines?country=us${query}&apiKey=${API_KEY}`
-				);
-				setArticles(response.data.articles);
-			} catch (error) {
-				console.log(error);
-			}
-			setLoading(false);
-		};
-
-		fetchData();
+	const [loading, response, error] = usePromise(() => {
+		const query = category === 'all' ? '' : `&category=${category}`;
+		return axios.get(
+			`https://newsapi.org/v2/top-headlines?country=us${query}&apiKey=${API_KEY}`
+		);
 	}, [category]);
 
 	// 대기 중일 경우
@@ -46,12 +31,17 @@ function NewsList({ category }) {
 		return <NewsListBlock>대기 중...</NewsListBlock>;
 	}
 
-	// 아직 articles 값이 설정되지 않았을 때
-	if (!articles) {
+	// 아직 response 값이 설정되지 않았을 때
+	if (!response) {
 		return null;
 	}
 
-	// articles 값이 유효할 때
+	if (error) {
+		return <NewsListBlock>에러 발생!</NewsListBlock>;
+	}
+
+	// response 값이 유효할 때
+	const { articles } = response.data;
 	return (
 		<NewsListBlock>
 			{articles.map((article) => (
